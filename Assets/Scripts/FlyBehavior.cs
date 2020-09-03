4601fb4b2d4ad9b7ab6cd3af1727535194b26023
Dan Anderson <11447979+Donkbonker@users.noreply.github.com>
2020-09-03 17:11:58 +0100

    Little bits

--- Assets/Scripts/FlyBehavior.cs
+++ Assets/Scripts/FlyBehavior.cs
@@ -4,17 +4,43 @@ using UnityEngine;
 
 [ExecuteInEditMode]
 public class FlyBehavior : MonoBehaviour {
-    private const float pi = Mathf.PI;
+    private const float TAU = 2 * Mathf.PI;
+
     private void Update() {
+        DrawRays();
+        ManualInputs();
+    }
+
+    private void ManualInputs() {
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Rotate(0, -0.5f, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            transform.Rotate(0, 0.5f, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W)) {
+            transform.Rotate(0.5f, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.S)) {
+            transform.Rotate(-0.5f, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.Space)) {
+            transform.Translate(transform.forward * 0.1f);
+        }
+    }
+
+    void DrawRays() {
         Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
-        int numRays = 2;
-        float stepSize = pi * 2 / numRays;
-        for (float x = -2*pi; x < 2*pi; x += stepSize) {
-            for (float y = -2*pi; y < 2*pi; y += stepSize) {
+        float stepSize = TAU / 2;
+        for (float x = -TAU; x < TAU; x += stepSize) {
+            for (float y = -TAU; y < TAU; y += stepSize) {
                 Vector3 newDir = forward + new Vector3(x, y, 0);
                 Debug.DrawRay(transform.position, newDir, Color.red);
             }
         }
-
     }
 }
